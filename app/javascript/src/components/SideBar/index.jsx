5d0302ecad00aa121a59e27ed93ea98fcd94ce74
Sankar N <sankarn@Sankars-MacBook-Pro.local>
2025-07-01 15:50:29 +0530

    Task 3 completed-1

--- app/javascript/src/components/SideBar/index.jsx
+++ app/javascript/src/components/SideBar/index.jsx
@@ -4,10 +4,10 @@ const Sidebar = () => (
   <div className="fixed left-0 top-0 flex h-screen w-16 flex-col items-center justify-between border-r bg-white py-6 shadow-sm">
     <div className="space-y-6">
       <div className="h-6 w-6 cursor-pointer">
-        <img src="https://img.icons8.com/?size=100&id=2797&format=png&color=000000" />
+        <img src="https://img.icons8.com/?size=100&id=tz1GQBtNqT2P&format=png&color=000000" />
       </div>
       <div className="h-6 w-6 cursor-pointer">
-        <img src="https://img.icons8.com/?size=100&id=8113&format=png&color=000000" />
+        <img src="https://img.icons8.com/?size=100&id=OTxpMqWbm71F&format=png&color=000000" />
       </div>
     </div>
     <div className="h-8 w-8 cursor-pointer">
--- app/javascript/src/components/commons/Container.jsx
+++ app/javascript/src/components/commons/Container.jsx
@@ -7,12 +7,12 @@ import PropTypes from "prop-types";
 import Sidebar from "../SideBar";
 
 const Container = ({ children, className = "" }) => (
-  <>
+  <div className="flex min-h-screen w-screen">
     <Sidebar />
-    <div className={classnames("mx-auto max-w-6xl px-6", [className])}>
+    <div className={classnames("ml-16 flex-1 px-6 py-6", [className])}>
       {children}
     </div>
-  </>
+  </div>
 );
 
 Container.propTypes = {
