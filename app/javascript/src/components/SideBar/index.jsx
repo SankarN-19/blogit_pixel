3af2d17642a800f0545e5a48046a04a61682bc8c
Sankar N <sankarn@Sankars-MacBook-Pro.local>
2025-07-10 15:17:17 +0530

    Added associations and Models for Task 6

--- app/javascript/src/components/SideBar/index.jsx
+++ app/javascript/src/components/SideBar/index.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 const Sidebar = () => (
   <div className="fixed left-0 top-0 flex h-screen w-16 flex-col items-center justify-between border-r bg-white py-6 shadow-sm">
     <div className="space-y-6">
-      <div className="h-6 w-6 cursor-pointer">
-        <img src="https://img.icons8.com/?size=100&id=tz1GQBtNqT2P&format=png&color=000000" />
-      </div>
+      <Link to="/dashboard">
+        <div className="h-6 w-6 cursor-pointer">
+          <img src="https://img.icons8.com/?size=100&id=tz1GQBtNqT2P&format=png&color=000000" />
+        </div>
+      </Link>
       <div className="h-6 w-6 cursor-pointer">
         <img src="https://img.icons8.com/?size=100&id=OTxpMqWbm71F&format=png&color=000000" />
       </div>
-      <div className="h-6 w-6 cursor-pointer">
-        <img src="https://img.icons8.com/?size=100&id=11762&format=png&color=000000" />
-      </div>
+      <Link to="/posts/create">
+        <div className="h-6 w-6 cursor-pointer pt-6">
+          <img src="https://img.icons8.com/?size=100&id=11762&format=png&color=000000" />
+        </div>
+      </Link>
     </div>
     <div className="h-8 w-8 cursor-pointer">
       <img src="https://img.icons8.com/?size=100&id=7819&format=png&color=000000" />
---
+++ app/models/organization.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Organization < ApplicationRecord
+  has_many :users, dependent: :destroy
+  validates :name, presence: true
+end
